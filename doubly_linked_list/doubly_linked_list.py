e976665169093157f2c4fea9dbc6f6559521e4ec
ItelSunday <iteldomingotech@gmail.com>
2019-06-24 16:53:13 -0700

    wip: linked-list

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -47,8 +47,18 @@ class DoublyLinkedList:
     pass
 
   def remove_from_head(self):
-    pass
-
+    if not self.head:
+      return None
+    if not self.head.length():
+      head = self.head
+      self.head = None
+      self.tail = None
+      return head.insert_before()
+    value = self.head.insert_before()
+    self.head = self.head.next()
+    return value
+      
+    
   def add_to_tail(self, value):
     pass
 
